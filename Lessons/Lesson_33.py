30d6e80e7730a28855ac9b03595ebd2bf146753e
Roman <yre8383@gmail.com>
2024-02-29 18:39:29 +0400

    new files

--- Lessons/Lesson_33.py
+++ Lessons/Lesson_33.py
@@ -112,37 +112,127 @@
 
 # # ----------------------------------------------------------------------------#
 
-def decorator_1(func):
-    print('Декоратор 1')
+# def decorator_1(func):
+#     print('Декоратор 1')
 
-    def wrapper():
-        print('перед функцией 1')
-        func()
+#     def wrapper():
+#         print('перед функцией 1')
+#         func()
 
-    return wrapper
-
-
-def decorator_2(func):
-    print('Декоратор 2')
-
-    def wrapper():
-        print('перед функцией 2')
-        func()
-
-    return wrapper
+#     return wrapper
 
 
-@decorator_1
-@decorator_2
-def basic_1():
-    print('basic_1')
+# def decorator_2(func):
+#     print('Декоратор 2')
+
+#     def wrapper():
+#         print('перед функцией 2')
+#         func()
+
+#     return wrapper
 
 
 # @decorator_1
-# def basic_2():
-#     print('basic_2')
+# @decorator_2
+# def basic_1():
+#     print('basic_1')
 
-print('>> start')
-basic_1()
-# basic_2()
-print('>> finish')
+
+# # @decorator_1
+# # def basic_2():
+# #     print('basic_2')
+
+# print('>> start')
+# basic_1()
+# # basic_2()
+# print('>> finish')
+# # ----------------------------------------------------------------------------#
+# Задание 1 и Задание 2
+# Создайтефункциюдляотображениятекущеговремени.
+# Функция не принимает параметров. Функция не исполь-
+# зуя синтаксис декораторов, произведите декорирование
+# функции с помощью другой функции. Потенциальный
+# вывод данных на экран:
+# ***************************
+# 23:00
+# ***************************
+# В этом выводе на экран две линии из звездочек – ре-
+# зультаты декорирования
+
+# from datetime import datetime
+
+
+# def decorator(fn):
+#     def wrapper():
+#         print('***************************')
+#         fn()
+#         print('***************************')
+#     return wrapper
+
+
+# def decorator_2(fn):
+#     def wrapper():
+#         fn()
+#         current_time = datetime.now()
+#         print(current_time.strftime('%Y-%m-%d'))
+#     return wrapper
+
+
+# @decorator
+# @decorator_2
+# def show_time():
+#     current_time = datetime.now()
+#     time = current_time.strftime('%H:%M:%S')
+#     print(time)
+
+
+# show_time()
+
+
+# # ----------------------------------------------------------------------------#
+# Задание 4
+# Создайте приложение по выпечке пиццы. Каждая
+# пицца содержит разные компоненты. Используя механизм
+# декораторов создайте разные пиццы:
+# ■ Маргарита;
+# ■ Четыре сыра;
+# ■ Капричоза;
+# ■ Гавайская.
+
+def pizza_margherita(fn):
+    def wrapper():
+        fn('Маргарита')
+        print('сыр, помидоры')
+    return wrapper
+
+
+def pizza_four_cheese(fn):
+    def wrapper():
+        fn('Четыре сыра')
+        print('моцарелла, пармезан, гауда, горгонзолла')
+    return wrapper
+
+
+def pizza_capricciosa(fn):
+    def wrapper():
+        fn('Капричоза')
+        print('моцарелла, ветчина, грибы, томаты, артишоки')
+    return wrapper
+
+
+def pizza_hawaiian(fn):
+    def wrapper():
+        fn('Гавайская')
+        print('куриное филе, ветчина, ананасы, моцарелла')
+    return wrapper
+
+
+@pizza_four_cheese
+# @pizza_margherita
+# @pizza_capricciosa
+# @pizza_hawaiian
+def pizza(name):
+    print(f'Пицца {name} содержит: тесто, кетчуп, ', end='')
+
+
+pizza()
